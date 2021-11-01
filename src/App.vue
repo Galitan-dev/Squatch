b126a8c2832783567b82323f005d1a47042819a0
Galitan-dev <galitan.dev@gmail.com>
2021-11-01 10:53:14 +0100

    transfer

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  --flame: #f1592aff;
+  --wheat: #ead2acff;
+  --gainsboro: #d1dedeff;
+  --rich-black-fogra-29: #0d1b2aff;
+  --oxford-blue: #1b263bff;
+  
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
